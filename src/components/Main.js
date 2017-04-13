7ded0f326212d8c0388cc5ae4be8fb8415287a31
Taylor McArthur <taydm.erek@gmail.com>
2017-04-13 10:03:15 -0700

    almost done

--- src/components/Main.js
+++ src/components/Main.js
@@ -28,18 +28,28 @@ export default React.createClass({
    },
 
 
+
 render (){
 	return(
-		<div className="shareDiv">
+		<div>
+		<div className="tabs">
+			<ul className="tabOptions">
+			<li className="tabOptionsLi"><a className="a" href="#">Overview</a></li>
+			<li className="tabOptionsLi"><a className="a" href="#">Repositories</a></li>
+			<li className="tabOptionsLi"><a className="a" href="#">Stars</a></li>
+			<li className="tabOptionsLi"><a className="a" href="#">Followers</a></li>
+			<li className="tabOptionsLi"><a className="a" href="#">Following</a></li>
+			</ul>
+		</div>
 		<div className="reposit">
 		{this.state.repo.map(repo=>(
 		<div key={'repo' + repo.id} className="repo">
 			<h3><a className="repoTitle" href={repo.svn_url}>{repo.name}</a></h3>
 			<div className="repoList">
-			<ul>
-				<li className="repoBullet1">{repo.language}</li>
-				<li className="repoBullet2">{repo.forks_count}</li>
-				<li className="repoBullet3">updated at {repo.updated_at}</li>
+			<ul className="flex">
+				<li className="repoBullet"> <input id="btn1" type="button" name="button" class="colorButton" value="" />{repo.language}</li>
+				<li className="repoBullet"> {repo.forks_count} </li>
+				<li className="repoBullet">updated at {repo.updated_at} </li>
 			</ul>
 			</div>
 			</div>
@@ -50,4 +60,6 @@ render (){
 		)
 	}
 })
+
+
 
